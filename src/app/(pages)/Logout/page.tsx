d90591af41ed84f3de977591103ace626bee0192
Warda Bounab <wardabounab@MacBook-Pro-de-Warda.local>
2024-12-11 01:41:30 +0100

    feat(client)

--- src/app/(pages)/Logout/page.tsx
+++ src/app/(pages)/Logout/page.tsx
@@ -1,15 +1,34 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 const Page = () => {
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
 
   useEffect(() => {
-    localStorage.removeItem("user");
-    router.push("/users");
+    const handleLogout = () => {
+      // Remove the user data from localStorage
+      localStorage.removeItem("user");
+
+      // Optional: If you're using an authentication service, call its logout method here
+      // For example:
+      // authService.logout();  // Example for Auth0 or Firebase
+
+      // Set loading to false after removing user data and completing logout tasks
+      setLoading(false);
+
+      // Redirect after logout
+      router.push("/users");
+    };
+
+    handleLogout();
   }, [router]);
+
+  if (loading) {
+    return <div>Logging out...</div>; // Display a loading message while the logout is in progress
+  }
 
   return <div></div>;
 };
